89507c860c5f1f485f6f6f38b1cc9eafdd3d6c36
Garrett Rowe <garrett.m.rowe@gmail.com>
2022-07-19 09:01:28 -0700

    Implement canvas.clj and associated tests

---
+++ src/racertay/canvas.clj
@@ -0,0 +1,73 @@
+(ns racertay.canvas
+  (:require [clojure.string :as s]
+            [racertay.tuple :refer :all]
+            ;[racertay.fcmp :refer :all]
+            [racertay.color :refer [color]]))
+
+
+(defn canvas [w h]
+  {:width w
+   :height h
+   :pixels (vec (repeat (* w h) (color 0 0 0)))})
+
+(defn fill-canvas [c color]
+  (let [new-pixels (map (constantly color) (:pixels c))]
+    (assoc c :pixels new-pixels)))
+
+(defn- int-in-range? [start end val]
+  (and (<= start val) (< val end)))
+
+(defn write-pixel [canvas x y color]
+  (if (and (int-in-range? 0 (:width canvas) x)
+           (int-in-range? 0 (:height canvas) y))
+    (let [index (+ (* (:width canvas) y) x)]
+      (assoc-in canvas [:pixels index] color))
+    canvas))
+
+(defn pixel-at [canvas x y]
+  (let [index (+ (* (:width canvas) y) x)]
+    (get-in canvas [:pixels index])))
+
+(defn clamp [value min max]
+  (cond
+    (< value min) min
+    (> value max) max
+    :else         value))
+
+(def max-subpixel-value 255)
+(def max-ppm-line-len 70)
+
+(defn- output-pixel
+  [p]
+  (letfn [(scale-subpixel [sub]
+            (-> (* max-subpixel-value sub)
+                (Math/round)
+                (clamp 0 max-subpixel-value)))]
+    (format "%s %s %s"
+            (scale-subpixel (:red p))
+            (scale-subpixel (:green p))
+            (scale-subpixel (:blue p)))))
+
+(defn- split-str-around-index [str index]
+  [(subs str 0 index)
+   (subs str (inc index))])
+
+(defn- output-line [line]
+  (s/join " " (map output-pixel line)))
+
+(defn- break-line [max-line-len line]
+  (if (< (count line) max-line-len)
+    line
+    (let [break-index (s/last-index-of line " " max-line-len)
+          [begin more] (split-str-around-index line break-index)
+          end (break-line max-line-len more)]
+      (format "%s%n%s" begin end))))
+
+(defn canvas-to-ppm [c]
+  (let [header (format "P3%n%s %s%n%s%n" (:width c) (:height c) max-subpixel-value)
+        pixel-data (->> (:pixels c)
+                        (partition (:width c))
+                        (map output-line)
+                        (map (partial break-line max-ppm-line-len))
+                        (s/join "\n"))]
+    (str header pixel-data \newline)))
---
+++ test/racertay/canvas_test.clj
@@ -0,0 +1,67 @@
+(ns racertay.canvas-test
+  (:require [clojure.test :refer :all]
+            [clojure.string :as s]
+            [racertay.canvas :refer :all]
+            [racertay.color :refer [color color-eq?]]))
+
+(deftest canvas-creation-test
+  (testing "a canvas has a width and a height"
+    (let [c (canvas 10 20)]
+      (is (= 10 (:width c)))
+      (is (= 20 (:height c)))))
+
+  (testing "all pixels of a canvas are initially black"
+    (let [c (canvas 10 20)
+          pixels (:pixels c)
+          black (color 0 0 0)]
+      (is (= 200 (count pixels)))
+      (is (every? #(color-eq? black %) pixels)))))
+
+(deftest canvas-writing-test
+  (testing "pixels of a canvas can be written to"
+    (let [init-canvas (canvas 10 20)
+          red (color 1 0 0)
+          c (write-pixel init-canvas 2 3 red)]
+      (is (color-eq? red (pixel-at c 2 3))))))
+
+(deftest canvas-to-ppm-test
+  (testing "canvas-to-ppm ouputs a proper header"
+    (let [c (canvas 5 3)
+          ppm (canvas-to-ppm c)
+          header (take 3 (s/split-lines ppm))]
+      (is (= ["P3" "5 3" "255"] header))))
+
+  (testing "canvas-to-ppm outputs proper pixel data"
+    (let [c1 (color 1.5 0 0)
+          c2 (color 0 0.5 0)
+          c3 (color -0.5 0 1)
+          c (-> (canvas 5 3)
+                (write-pixel 0 0 c1)
+                (write-pixel 2 1 c2)
+                (write-pixel 4 2 c3))
+          ppm (canvas-to-ppm c)
+          pixel-data (->> (s/split-lines ppm)
+                          (drop 3) ; drop the header
+                          (take 3))]
+      (is (= ["255 0 0 0 0 0 0 0 0 0 0 0 0 0 0"
+              "0 0 0 0 0 0 0 128 0 0 0 0 0 0 0"
+              "0 0 0 0 0 0 0 0 0 0 0 0 0 0 255"]
+             pixel-data))))
+
+  (testing "long lines in ppm file should be split"
+    (let [c (fill-canvas (canvas 10 2) (color 1.0 0.8 0.6))
+          ppm (canvas-to-ppm c)
+          pixel-data (->> (s/split-lines ppm)
+                          (drop 3) ; drop the header
+                          (take 4))]
+      (is (= ["255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204"
+              "153 255 204 153 255 204 153 255 204 153 255 204 153"
+              "255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204"
+              "153 255 204 153 255 204 153 255 204 153 255 204 153"]
+             pixel-data))))
+
+  (testing "ppm file must end in newline character"
+    (let [c (canvas 5 3)
+          ppm (canvas-to-ppm c)]
+      (is (= \newline (last ppm))))))
+
