a7cba837f476669f188af117b6997a790c222222
Garrett Rowe <garrett.m.rowe@gmail.com>
2022-07-22 23:11:32 -0700

    Update rendering to use p6 ppm format

--- src/racertay/core.clj
+++ src/racertay/core.clj
@@ -1,12 +1,14 @@
 (ns racertay.core
-  (:require [racertay.tuple :refer [point x y]]
+  (:require [clojure.java.io :as io]
+            [racertay.tuple :refer :all]
             [racertay.canvas :refer :all]
             [racertay.transformations :refer :all]
             [racertay.matrix :refer :all]
-            [racertay.color :refer [color]])
+            [racertay.color :refer [color]]
+            [racertay.sphere :as sphere]
+            [racertay.ray :as ray]
+            [racertay.intersection :as intersect])
   (:gen-class))
-
-
 
 (defn clock-face [clock-radius]
   (let [hours 12
@@ -28,7 +30,36 @@
         canvas-space (map #(mat-mul-tup canvas-translation %) (clock-face clock-radius))]
     (reduce (fn [c p] (write-pixel c (int (x p)) (int (y p)) black)) init-canvas canvas-space)))
 
+(defn canvas-indices [canvas]
+  (for [y (range 0 (:height canvas))
+        x (range 0 (:width canvas))]
+    [x y]))
+
+(def sphere-canvas
+  (let [width 100
+        canvas (canvas width width)
+        s (sphere/sphere)
+        ray-source (point 0 0 -5)
+        wall-z 10.0
+        wall-size 15.0
+        pixel-size (/ wall-size width)
+        half-wall (/ wall-size 2)
+        sphere-color (color 1 0 0)
+        f (fn [canvas [x y]]
+            (let [world-y (- half-wall (* pixel-size y))
+                  world-x (+ (- half-wall) (* pixel-size x))
+                  wall-position (point world-x world-y wall-z)
+                  ray-direction (normalize (tup-sub wall-position ray-source))
+                  r (ray/ray ray-source ray-direction)
+                  xs (sphere/intersect s r)]
+              (if (intersect/hit xs)
+                (write-pixel canvas x y sphere-color)
+                canvas)))]
+    (reduce f canvas (canvas-indices canvas))))
+
 (defn -main
   [& args]
-  (let [filename "output.ppm"]
-    (spit filename (canvas-to-ppm (clock-canvas 200 200 50)))))
+  (let [filename "output2.ppm"
+        ppm (canvas-to-p6-ppm sphere-canvas)]
+    (with-open [out (io/output-stream filename)]
+      (.write out ppm))))
