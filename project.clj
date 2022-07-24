cdf16b3775ff2c0b2612a8b9079cc545f5853011
Garrett Rowe <garrett.m.rowe@gmail.com>
2022-07-24 13:07:31 -0700

    Update project description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject racertay "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "A small ray tracer based on The Ray Tracer Challenge"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
@@ -7,5 +7,4 @@
   :main ^:skip-aot racertay.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
-                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}
-             :dev {:dependencies [[org.clojure/tools.namespace "1.3.0"]]}})
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
