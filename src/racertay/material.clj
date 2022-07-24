4ada86e66a3895aae16c7cc9f22380b1ce08b179
Garrett Rowe <garrett.m.rowe@gmail.com>
2022-07-24 14:51:15 -0700

    Implement mutators and equality testing

--- src/racertay/material.clj
+++ src/racertay/material.clj
@@ -1,5 +1,6 @@
 (ns racertay.material
-  (:require [racertay.color :as color]))
+  (:require [racertay.color :as color]
+            [racertay.fcmp :as fcmp]))
 
 (defn material []
   {:color (color/color 1 1 1)
@@ -11,14 +12,37 @@
 (defn color [material]
   (:color material))
 
+(defn assoc-color [material new-color]
+  (assoc material :color new-color))
+
 (defn ambient [material]
   (:ambient material))
+
+(defn assoc-ambient [material new-ambient]
+  (assoc material :ambient new-ambient))
 
 (defn diffuse [material]
   (:diffuse material))
 
+(defn assoc-diffuse [material new-diffuse]
+  (assoc material :diffuse new-diffuse))
+
 (defn specular [material]
   (:specular material))
 
+(defn assoc-specular [material new-specular]
+  (assoc material :specular new-specular))
+
 (defn shininess [material]
   (:shininess material))
+
+(defn assoc-shininess [material new-shininess]
+  (assoc material :shininess new-shininess))
+
+(defn material-eq? [mat1 mat2]
+  (and
+   (color/color-eq? (color mat1) (color mat2))
+   (fcmp/nearly-eq? (ambient mat1) (ambient mat2))
+   (fcmp/nearly-eq? (diffuse mat1) (diffuse mat2))
+   (fcmp/nearly-eq? (specular mat1) (specular mat2))
+   (fcmp/nearly-eq? (shininess mat1) (shininess mat2))))
--- test/racertay/material_test.clj
+++ test/racertay/material_test.clj
@@ -19,3 +19,36 @@
 
   (testing "Material has default shininess attribute"
     (is (fcmp/nearly-eq? 200.0 (shininess (material))))))
+
+(deftest material-assoc-test
+  (testing "Material color can be assoc'ed"
+    (let [new-color (color/color 0 1 1)
+          material (assoc-color (material) new-color)]
+      (is (color/color-eq? new-color (color material)))))
+
+  (testing "Material ambient attribute can be assoc'ed"
+    (let [new-ambient 0.5
+          material (assoc-ambient (material) new-ambient)]
+      (is (fcmp/nearly-eq? new-ambient (ambient material)))))
+
+  (testing "Material diffuse attribute can be assoc'ed"
+    (let [new-diffuse 0.5
+          material (assoc-diffuse (material) new-diffuse)]
+      (is (fcmp/nearly-eq? new-diffuse (diffuse material)))))
+
+  (testing "Material specular attribute can be assoc'ed"
+    (let [new-specular 0.5
+          material (assoc-specular (material) new-specular)]
+      (is (fcmp/nearly-eq? new-specular (specular material)))))
+
+  (testing "Material shininess attribute can be assoc'ed"
+    (let [new-shininess 300.0
+          material (assoc-shininess (material) new-shininess)]
+      (is (fcmp/nearly-eq? new-shininess (shininess material))))))
+
+(deftest material-eq?-test
+  (testing "material-eq? returns true for two default materials"
+    (is (material-eq? (material) (material))))
+
+  (testing "material-eq? returns false when material is not equal"
+    (is (not (material-eq? (material) (assoc-shininess (material) 300.0))))))
