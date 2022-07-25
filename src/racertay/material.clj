19362a7fd05cb2cf7475831560bbe2ac4a75aa86
Garrett Rowe <garrett.m.rowe@gmail.com>
2022-07-24 17:50:15 -0700

    Implement lighting function with tests

--- src/racertay/material.clj
+++ src/racertay/material.clj
@@ -1,6 +1,8 @@
 (ns racertay.material
   (:require [racertay.color :as color]
-            [racertay.fcmp :as fcmp]))
+            [racertay.fcmp :as fcmp]
+            [racertay.light :as light]
+            [racertay.tuple :as tup]))
 
 (defn material []
   {:color (color/color 1 1 1)
@@ -46,3 +48,33 @@
    (fcmp/nearly-eq? (diffuse mat1) (diffuse mat2))
    (fcmp/nearly-eq? (specular mat1) (specular mat2))
    (fcmp/nearly-eq? (shininess mat1) (shininess mat2))))
+
+(defn- calc-ambient [effective-color material]
+  (color/color-mul-scalar effective-color (ambient material)))
+
+(defn- calc-diffuse [light-dot-normal effective-color material]
+  (if (neg? light-dot-normal)
+    (color/color 0 0 0)
+    (color/color-mul-scalar
+     effective-color (* (diffuse material) light-dot-normal))))
+
+(defn- calc-specular [light-dot-normal normalv lightv eyev light material]
+  (if (neg? light-dot-normal)
+    (color/color 0 0 0)
+    (let [reflectv (tup/reflect (tup/tup-neg lightv) normalv)
+          reflect-dot-eye (tup/dot reflectv eyev)]
+      (if (pos? reflect-dot-eye)
+        (color/color-mul-scalar
+         (light/intensity light)
+         (* (specular material)
+            (Math/pow reflect-dot-eye (shininess material))))
+        (color/color 0 0 0)))))
+
+(defn lighting [material light point eyev normalv]
+  (let [effective-color (color/color-mul (color material) (light/intensity light))
+        lightv (tup/normalize (tup/tup-sub (light/position light) point))
+        light-dot-normal (tup/dot lightv normalv)]
+    (color/color-add
+     (calc-ambient effective-color material)
+     (calc-diffuse light-dot-normal effective-color material)
+     (calc-specular light-dot-normal normalv lightv eyev light material))))
--- test/racertay/material_test.clj
+++ test/racertay/material_test.clj
@@ -2,7 +2,9 @@
   (:require [clojure.test :refer :all]
             [racertay.material :refer :all]
             [racertay.color :as color]
-            [racertay.fcmp :as fcmp]))
+            [racertay.fcmp :as fcmp]
+            [racertay.tuple :as tup]
+            [racertay.light :as light]))
 
 (deftest material-creation-test
   (testing "Material has a default color"
@@ -52,3 +54,48 @@
 
   (testing "material-eq? returns false when material is not equal"
     (is (not (material-eq? (material) (assoc-shininess (material) 300.0))))))
+
+(deftest lighting-test
+  (let [m (material)
+        surface-pos (tup/point 0 0 0)]
+    (testing "Lighting with the eye between the light and the surface"
+      (let [eyev (tup/vect 0 0 -1)
+            normalv (tup/vect 0 0 -1)
+            light (light/point-light (tup/point 0 0 -10) (color/color 1 1 1))]
+        (is (color/color-eq?
+             (color/color 1.9 1.9 1.9)
+             (lighting m light surface-pos eyev normalv)))))
+
+    (testing "Lighting with eye betweem the light and surface - eye offset 45 deg"
+      (let [rad-2-over-2 (/ (Math/sqrt 2) 2)
+            eyev (tup/vect 0 rad-2-over-2 (- rad-2-over-2))
+            normalv (tup/vect 0 0 -1)
+            light (light/point-light (tup/point 0 0 -10) (color/color 1 1 1))]
+        (is (color/color-eq?
+             (color/color 1 1 1)
+             (lighting m light surface-pos eyev normalv)))))
+
+    (testing "Lighting with eye opposite surface - light offset 45 deg"
+      (let [eyev (tup/vect 0 0 -1)
+            normalv (tup/vect 0 0 -1)
+            light (light/point-light (tup/point 0 10 -10) (color/color 1 1 1))]
+        (is (color/color-eq?
+             (color/color 0.7364 0.7364 0.7364)
+             (lighting m light surface-pos eyev normalv)))))
+
+    (testing "Lighting with eye in path of reflection vector"
+      (let [rad-2-over-2 (/ (Math/sqrt 2) 2)
+            eyev (tup/vect 0 (- rad-2-over-2) (- rad-2-over-2))
+            normalv (tup/vect 0 0 -1)
+            light (light/point-light (tup/point 0 10 -10) (color/color 1 1 1))]
+        (is (color/color-eq?
+             (color/color 1.6364 1.6364 1.6364)
+             (lighting m light surface-pos eyev normalv)))))
+
+    (testing "Lighting with light behind surface"
+      (let [eyev (tup/vect 0 0 -1)
+            normalv (tup/vect 0 0 -1)
+            light (light/point-light (tup/point 0 0 10) (color/color 1 1 1))]
+        (is (color/color-eq?
+             (color/color 0.1 0.1 0.1)
+             (lighting m light surface-pos eyev normalv)))))))
