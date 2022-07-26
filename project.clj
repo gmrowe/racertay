e002ebbb7ae122ccfb9d0739c1caa6c4f479f11e
Garrett Rowe <garrett.m.rowe@gmail.com>
2022-07-25 22:20:24 -0700

    Re-add dev dependencies

--- project.clj
+++ project.clj
@@ -7,4 +7,5 @@
   :main ^:skip-aot racertay.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
-                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}
+             :dev {:dependencies [[org.clojure/tools.namespace "1.3.0"]]}})
