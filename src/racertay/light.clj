6f28c403a26434b2e7d6b5e5477a9c8dd597fe17
Garrett Rowe <garrett.m.rowe@gmail.com>
2022-07-26 21:17:22 -0700

    Implement racertay.light/eq? with tests

--- src/racertay/light.clj
+++ src/racertay/light.clj
@@ -1,4 +1,6 @@
-(ns racertay.light)
+(ns racertay.light
+  (:require [racertay.tuple :as tup]
+            [racertay.color :as color]))
 
 (defn point-light [position intensity]
   {:position position
@@ -10,4 +12,8 @@
 (defn intensity [light]
   (:intensity light))
 
+(defn eq? [light1 light2]
+  (and
+   (tup/tup-eq? (:position light1) (:position light2))
+   (color/color-eq? (:intensity light1) (:intensity light2))))
 
--- test/racertay/light_test.clj
+++ test/racertay/light_test.clj
@@ -4,10 +4,27 @@
             [racertay.color :as color]
             [racertay.tuple :as tup]))
 
-(deftest point-light-test
-    (testing "A point light has a position and intensity"
-      (let [p (tup/point 0 0 0)            
-            i (color/color 1 1 1)
-            light (point-light p i)]
-        (is (color/color-eq? i (intensity light)))
-        (is (tup/tup-eq? p (position light))))))
+(deftest point-light-creation-test
+  (testing "A point light has a position and intensity"
+    (let [p (tup/point 0 0 0)            
+          i (color/color 1 1 1)
+          light (point-light p i)]
+      (is (color/color-eq? i (intensity light)))
+      (is (tup/tup-eq? p (position light))))))
+
+(deftest light-eq?-test
+  (testing "Lights with different positions are not equal"
+    (let [color (color/color 1 1 1)]
+      (is (not (eq? (point-light (tup/point 0 0 0) color)
+                    (point-light (tup/point 0 0 1) color))))))
+
+  (testing "Lights with different colors are not equal"
+    (let [position (tup/point 0 0 0)]
+      (is (not (eq? (point-light position (color/color 0 0 0))
+                    (point-light position (color/color 1 0 0)))))))
+
+  (testing "Lights with same position and color are equal"
+    (let [color (color/color 1 1 1)
+          position (tup/point 0 0 0)]
+      (is (eq? (point-light position color) (point-light position color))))))
+
