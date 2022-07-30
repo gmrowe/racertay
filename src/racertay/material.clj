ede54427a67501e645a5dc92f0616ccbbb4e058f
Garrett Rowe <garrett.m.rowe@gmail.com>
2022-07-30 09:14:40 -0700

    Use qualifying map syntax in material.clj

--- src/racertay/material.clj
+++ src/racertay/material.clj
@@ -5,11 +5,11 @@
             [racertay.tuple :as tup]))
 
 (def new-material
-  {:material/color (color/color 1 1 1)
-   :material/ambient 0.1
-   :material/diffuse 0.9
-   :material/specular 0.9
-   :material/shininess 200.0})
+  #:material{:color (color/color 1 1 1)
+             :ambient 0.1
+             :diffuse 0.9
+             :specular 0.9
+             :shininess 200.0})
 
 (defn assoc-shininess [material new-shininess]
   (assoc material :shininess new-shininess))
