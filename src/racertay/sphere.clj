540a7a12e9979612764a63c113ed67a698e253d9
Garrett Rowe <garrett.m.rowe@gmail.com>
2022-07-30 06:53:18 -0700

    Remove functions that were commented out

--- src/racertay/sphere.clj
+++ src/racertay/sphere.clj
@@ -16,7 +16,6 @@
 
 (def world-origin (tup/point 0 0 0))
 
-
 (defrecord ISphere
   [id transform inverse-transform material]
   p/Shape
@@ -48,26 +47,3 @@
    matrix/identity-matrix
    matrix/identity-matrix
    material/new-material))
-
-
-;; (defn intersect [sphere ray] 
-;;   (let [r (ray/transform ray (:inverse-transform sphere))
-;;         sphere-to-ray-vec (tup/tup-sub (ray/origin r) world-origin)
-;;         a (tup/dot (ray/direction r) (ray/direction r))
-;;         b (* 2 (tup/dot (ray/direction r) sphere-to-ray-vec))
-;;         c (dec (tup/dot sphere-to-ray-vec sphere-to-ray-vec))
-;;         discriminant (- (* b b) (* 4 a c))]
-;;     (if (neg? discriminant)
-;;       inter/empty-intersections
-;;       (inter/intersections
-;;        (inter/intersection (/ (- (- b) (Math/sqrt discriminant)) (* a 2)) sphere)
-;;        (inter/intersection (/ (+ (- b) (Math/sqrt discriminant)) (* a 2)) sphere)))))
-
-;; (defn normal-at [sphere point]
-;;   (let [object-point (matrix/mat-mul-tup (:inverse-transform sphere) point)
-;;         object-normal (tup/tup-sub object-point world-origin)
-;;         world-normal (matrix/mat-mul-tup
-;;                       (matrix/transpose (:inverse-transform sphere))
-;;                       object-normal)]
-;;     (tup/normalize
-;;      (tup/vect (tup/x world-normal) (tup/y world-normal) (tup/z world-normal)))))
