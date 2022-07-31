65b7352e04ab83ba4529a07d8fff3ef30711ff89
Garrett Rowe <garrett.m.rowe@gmail.com>
2022-07-31 16:56:02 -0700

    Implement example image from chaper 7

--- src/racertay/core.clj
+++ src/racertay/core.clj
@@ -1,104 +1,99 @@
 (ns racertay.core
   (:require [clojure.java.io :as io]
-            [racertay.tuple :refer :all]
-            [racertay.canvas :refer :all]
-            [racertay.transformations :refer :all]
-            [racertay.matrix :refer :all]
-            [racertay.color :refer [color]]
+            [racertay.tuple :as tup]
+            [racertay.color :as col]
             [racertay.sphere :as sphere]
-            [racertay.ray :as ray]
-            [racertay.intersection :as intersect]
-            [racertay.material :as material]
+            [racertay.transformations :as xform]
+            [racertay.world :as wor]
             [racertay.light :as light]
-            [racertay.protocols :as p])
+            [racertay.camera :as cam]
+            [racertay.canvas :as canv])
   (:gen-class))
 
-(defn clock-face [clock-radius]
-  (let [hours 12
-        origin (point 0 0 0)
-        noon (point 0 1 0)
-        scale (scaling 1 clock-radius 1)
-        rotation-angle (/ (* Math/PI 2) hours)
-        rotate (fn [n] (rotation-z (* n rotation-angle)))]
-    (map (fn [n]
-           (mat-mul-tup (mat-mul (rotate n) scale (translation 0 1 0)) origin))
-         (range 0 hours))))
 
-(def black (color 0 0 0))
-(def white (color 1 1 1))
+(def floor
+  (-> (sphere/sphere)
+      (sphere/apply-transform (xform/scaling 10 0.01 10))
+      (assoc-in [:material :material/color] (col/color 1 0.9 0.9))
+      (assoc-in [:material :material/specular] 0)))
 
-(defn clock-canvas [width height clock-radius]
-  (let [init-canvas (canvas width height white)
-        canvas-translation (translation (/ width 2) (/ height 2) 0)
-        canvas-space (map #(mat-mul-tup canvas-translation %) (clock-face clock-radius))]
-    (reduce (fn [c p] (write-pixel c (int (x p)) (int (y p)) black)) init-canvas canvas-space)))
+(def left-wall
+  (-> (sphere/sphere)
+      (sphere/apply-transform
+       (xform/scaling 10 0.01 10)
+       (xform/rotation-x (/ Math/PI 2))
+       (xform/rotation-y (- (/ Math/PI 4)))
+       (xform/translation 0 0 5))
+      (assoc :material (:material floor))))
 
-(defn canvas-indices [canvas]
-  (for [y (range 0 (:height canvas))
-        x (range 0 (:width canvas))]
-    [x y]))
+(def right-wall
+  (-> (sphere/sphere)
+      (sphere/apply-transform
+       (xform/scaling 10 0.01 10)
+       (xform/rotation-x (/ Math/PI 2))
+       (xform/rotation-y (/ Math/PI 4))
+       (xform/translation 0 0 5))
+      (assoc :material (:material floor))))
 
-(defn red-circle-canvas [width]
-  (let [canvas (canvas width width)
-        xform (mat-mul
-               (rotation-z (/ Math/PI 6))
-               (scaling 1.3 0.3 1))
-        s (sphere/apply-transform (sphere/sphere) xform)
-        ray-source (point 0 0 -5)
-        wall-z 10.0
-        wall-size 10.0
-        pixel-size (/ wall-size width)
-        half-wall (/ wall-size 2)
-        sphere-color (color 1 0 0)
-        f (fn [canvas [x y]]
-            (let [world-y (- half-wall (* pixel-size y))
-                  world-x (+ (- half-wall) (* pixel-size x))
-                  wall-position (point world-x world-y wall-z)
-                  ray-direction (normalize (tup-sub wall-position ray-source))
-                  r (ray/ray ray-source ray-direction)
-                  xs (p/intersect s r)]
-              (if (intersect/hit xs)
-                (write-pixel canvas x y sphere-color)
-                canvas)))]
-    (reduce f canvas (canvas-indices canvas))))
+(def middle-sphere
+  (-> (sphere/sphere)
+      (sphere/apply-transform
+       (xform/translation -0.5 1 0.5))
+      (assoc-in [:material :material/color] (col/color 0.1 1 0.5))
+      (assoc-in [:material :material/diffuse] 0.7)
+      (assoc-in [:material :material/specular] 0.3)))
 
-(defn sphere-canvas [width]
-  (let [canvas (canvas width width)
-        sphere-color (color 1 1 0)
-        scale (scaling 1.3 0.3 1)
-        rot (rotation-z (/ Math/PI 6))
-        m (assoc material/new-material :material/color sphere-color)
-        s (-> (sphere/sphere)
-              (assoc :material m)
-              (sphere/apply-transform scale rot))
-        ray-source (point 0 0 -5)
-        wall-z 10.0
-        wall-size 10.0
-        pixel-size (/ wall-size width)
-        half-wall (/ wall-size 2)
-        light-color (color 1 1 1)
-        light-pos (point -10 10 -10)
-        light (light/point-light light-pos light-color)
-        f (fn [canvas [x y]]
-            (let [world-y (- half-wall (* pixel-size y))
-                  world-x (+ (- half-wall) (* pixel-size x))
-                  wall-position (point world-x world-y wall-z)
-                  ray-direction (normalize (tup-sub wall-position ray-source))
-                  r (ray/ray ray-source ray-direction)
-                  xs (p/intersect s r)]
-              (if-let [i (intersect/hit xs)]
-                (let [point (ray/position r (:intersection/t i))
-                      normal (p/normal-at (:intersection/object i) point)
-                      eye (tup-neg (:ray/direction r))
-                      material (:material (:intersection/object i))
-                      color (material/lighting material light point eye normal)]
-                  (write-pixel canvas x y color))
-                canvas)))]
-    (reduce f canvas (canvas-indices canvas))))
+(def right-sphere
+  (-> (sphere/sphere)
+      (sphere/apply-transform
+       (xform/scaling 0.5 0.5 0.5)
+       (xform/translation 1.5 0.5 -0.5))
+      (assoc-in [:material :material/color] (col/color 0.5 1 0.1))
+      (assoc-in [:material :material/diffuse] 0.7)
+      (assoc-in [:material :material/specular] 0.3)))
 
-(defn -main
-  [& args]
-  (let [filename "output3.ppm"
-        ppm (canvas-to-p6-ppm (sphere-canvas 500))]
+(def left-sphere
+  (-> (sphere/sphere)
+      (sphere/apply-transform
+       (xform/scaling 0.33 0.33 0.33)
+       (xform/translation -1.5 0.33 -0.75))
+      (assoc-in [:material :material/color] (col/color 1 0.8 0.1))
+      (assoc-in [:material :material/diffuse] 0.7)
+      (assoc-in [:material :material/specular] 0.3)))
+
+(def light
+  (light/point-light
+   (tup/point -10 10 -10) (col/color 1 1 1)))
+
+(def width 600)
+(def height 450)
+(def field-of-view (/ Math/PI 3))
+(def camera-location (tup/point 0 1.5 -5))
+(def canvas-location (tup/point 0 1 0))
+(def up (tup/vect 0 1 0))
+
+(def camera
+  (-> (cam/camera width height field-of-view)
+      (cam/apply-transform
+       (xform/view-transform camera-location canvas-location up))))
+
+(def world
+  (-> wor/empty-world
+      (assoc :world/light light)
+      (update :world/objects conj floor)
+      (update :world/objects conj left-wall)
+      (update :world/objects conj right-wall)
+      (update :world/objects conj middle-sphere)
+      (update :world/objects conj right-sphere)
+      (update :world/objects conj left-sphere)))
+
+(defn render-canvas-to-file [canvas filename]
+  (let [ppm (canv/canvas-to-p6-ppm canvas)]
     (with-open [out (io/output-stream filename)]
       (.write out ppm))))
+
+(defn -main [& args]
+  (let [canvas (cam/render camera world)]
+    (render-canvas-to-file canvas "output.ppm")))
+
+
