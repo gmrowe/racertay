68f499b3e531a2f6195e080bc9e010abb9c48542
Garrett Rowe <garrett.m.rowe@gmail.com>
2022-08-06 15:49:39 -0700

    Add color contants to color.clj

--- src/racertay/color.clj
+++ src/racertay/color.clj
@@ -6,6 +6,18 @@
    :green (double g)
    :blue (double b)})
 
+(def black (color 0 0 0))
+(def white (color 1 1 1))
+(def red (color 1 0 0))
+(def green (color 0 1 0))
+(def blue (color 0 0 1))
+(def aqua (color 0 1 1))
+(def yellow (color 1 1 0))
+(def fuchsia (color 1 0 1))
+(def pink (color 1 0.71 0.77))
+(def orange (color 1 0.65 0))
+(def indigo (color 0.29 0 0.51))
+
 (defn color-eq? [c1 c2]
   (and
    (nearly-eq? (:red c1) (:red c2))
