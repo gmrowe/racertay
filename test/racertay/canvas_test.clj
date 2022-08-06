ea9b393a91b3ee4494ab930322bd032dc152cd02
Garrett Rowe <garrett.m.rowe@gmail.com>
2022-08-06 08:44:34 -0700

    Refactor tests in canvas_test.clj for clarity

--- test/racertay/canvas_test.clj
+++ test/racertay/canvas_test.clj
@@ -31,40 +31,41 @@
       (is (color-eq? red (pixel-at c 2 3))))))
 
 (deftest canvas-to-ppm-test
-  (testing "canvas-to-ppm ouputs a proper header"
-    (let [c (canvas 5 3)
-          ppm (canvas-to-ppm c)
-          header (take 3 (s/split-lines ppm))]
-      (is (= ["P3" "5 3" "255"] header))))
+  (let [header-line-count 3]
+    (testing "canvas-to-ppm ouputs a proper header"
+      (let [c (canvas 5 3)
+            ppm (canvas-to-ppm c)
+            header (take header-line-count (s/split-lines ppm))]
+        (is (= ["P3" "5 3" "255"] header))))
 
-  (testing "canvas-to-ppm outputs proper pixel data"
-    (let [c1 (color 1.5 0 0)
-          c2 (color 0 0.5 0)
-          c3 (color -0.5 0 1)
-          c (-> (canvas 5 3)
-                (write-pixel 0 0 c1)
-                (write-pixel 2 1 c2)
-                (write-pixel 4 2 c3))
-          ppm (canvas-to-ppm c)
-          pixel-data (->> (s/split-lines ppm)
-                          (drop 3) ; drop the header
-                          (take 3))]
-      (is (= ["255 0 0 0 0 0 0 0 0 0 0 0 0 0 0"
-              "0 0 0 0 0 0 0 128 0 0 0 0 0 0 0"
-              "0 0 0 0 0 0 0 0 0 0 0 0 0 0 255"]
-             pixel-data))))
+    (testing "canvas-to-ppm outputs proper pixel data"
+      (let [c1 (color 1.5 0 0)
+            c2 (color 0 0.5 0)
+            c3 (color -0.5 0 1)
+            c (-> (canvas 5 3)
+                  (write-pixel 0 0 c1)
+                  (write-pixel 2 1 c2)
+                  (write-pixel 4 2 c3))
+            ppm (canvas-to-ppm c)
+            pixel-data (->> (s/split-lines ppm)
+                            (drop header-line-count) ; drop the header
+                            (take 3))]
+        (is (= ["255 0 0 0 0 0 0 0 0 0 0 0 0 0 0"
+                "0 0 0 0 0 0 0 128 0 0 0 0 0 0 0"
+                "0 0 0 0 0 0 0 0 0 0 0 0 0 0 255"]
+               pixel-data))))
 
-  (testing "long lines in ppm file should be split"
-    (let [c (canvas 10 2 (color 1.0 0.8 0.6))
-          ppm (canvas-to-ppm c)
-          pixel-data (->> (s/split-lines ppm)
-                          (drop 3)    ; drop the header
-                          (take 4))]
-      (is (= ["255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204"
-              "153 255 204 153 255 204 153 255 204 153 255 204 153"
-              "255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204"
-              "153 255 204 153 255 204 153 255 204 153 255 204 153"]
-             pixel-data))))
+    (testing "long lines in ppm file should be split"
+      (let [c (canvas 10 2 (color 1.0 0.8 0.6))
+            ppm (canvas-to-ppm c)
+            pixel-data (->> (s/split-lines ppm)
+                            (drop header-line-count)    ; drop the header
+                            (take 4))]
+        (is (= ["255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204"
+                "153 255 204 153 255 204 153 255 204 153 255 204 153"
+                "255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204"
+                "153 255 204 153 255 204 153 255 204 153 255 204 153"]
+               pixel-data)))))
 
   (testing "ppm file must end in newline character"
     (let [c (canvas 5 3)
@@ -81,7 +82,7 @@
   (testing "canvas-to-p6-ppm outputs correct pixel data"
     (let [c (canvas 2 1 (color 1.5 0.5 -0.1))
           bytes-per-pixel 3
-          byte-count (* bytes-per-pixel (count (:canvas/pixels c)))
+          pixel-byte-count (* bytes-per-pixel (count (:canvas/pixels c)))
           ppm (vec (canvas-to-p6-ppm c))
-          pixel-data (drop (- (count ppm) byte-count) ppm)]
+          pixel-data (take-last pixel-byte-count ppm)]
       (is (= [-1 -128 0 -1 -128 0] pixel-data)))))
