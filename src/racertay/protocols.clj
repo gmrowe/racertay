ede60b2ae6d1c983f67e7bd1da4b37798f7bbac5
Garrett Rowe <garrett.m.rowe@gmail.com>
2022-08-14 11:21:12 -0700

    Add reflective property to material in shape-data

--- src/racertay/protocols.clj
+++ src/racertay/protocols.clj
@@ -16,7 +16,8 @@
                         :ambient 0.1
                         :diffuse 0.9
                         :specular 0.9
-                        :shininess 200.0}})
+                        :shininess 200.0
+                        :reflective 0.0}})
 
 (defn apply-transform
   ([obj xform]
