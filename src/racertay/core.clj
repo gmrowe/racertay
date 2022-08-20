61e9d72f428769b62cde2f11b1505cb3c2fff23b
Garrett Rowe <garrett.m.rowe@gmail.com>
2022-08-20 09:45:41 -0700

    Play around with reflection and transparency

--- src/racertay/core.clj
+++ src/racertay/core.clj
@@ -17,7 +17,7 @@
   (-> (shape/plane)
       (assoc-in [:material :material/color] (col/color 1 0.9 0.9))
       (assoc-in [:material :material/specular] 0)
-      (assoc-in [:material :material/reflective] 0.25)))
+      (assoc-in [:material :material/reflective] 0.85)))
 
 (def left-wall
   (-> (shape/plane)
@@ -45,8 +45,18 @@
       (assoc-in [:material :material/pattern]
                 (chevron-pattern col/orange col/green))))
 
+(def left-sphere
+  (-> (shape/sphere)
+      (shape/apply-transform
+       (xform/scaling 0.33 0.33 0.33)
+       (xform/translation -1.5 0.33 -0.75))
+      (assoc-in [:material :material/color] col/dark-blue)
+      (assoc-in [:material :material/diffuse] 0.7)
+      (assoc-in [:material :material/specular] 0.3)
+      (assoc-in [:materail :material/reflective] 0.15)))
+
 (def middle-sphere
-  (let [pattern (-> (patt/gradient-pattern col/violet col/beige)
+  (let [pattern (-> (patt/stripe-pattern col/violet col/white)
                     (shape/apply-transform (xform/scaling 0.2 0.2 0.2)
                                        (xform/rotation-z (/ Math/PI 3))))]
     (-> (shape/sphere)
@@ -60,25 +70,20 @@
       (shape/apply-transform
        (xform/scaling 0.5 0.5 0.5)
        (xform/translation 1.5 0.5 -0.5))
-      (assoc-in [:material :material/color] col/dark-red)
-      (assoc-in [:material :material/diffuse] 0.7)
-      (assoc-in [:material :material/specular] 0.3)))
-
-(def left-sphere
-  (-> (shape/sphere)
-      (shape/apply-transform
-       (xform/scaling 0.33 0.33 0.33)
-       (xform/translation -1.5 0.33 -0.75))
-      (assoc-in [:material :material/color] col/gold)
-      (assoc-in [:material :material/diffuse] 0.7)
-      (assoc-in [:material :material/specular] 0.3)))
+      (assoc-in [:material :material/color] col/black)
+      (assoc-in [:material :material/diffuse] 0.5)
+      (assoc-in [:material :material/ambient] 0.4)
+      (assoc-in [:material :material/specular] 0.4)
+      (assoc-in [:material :material/transparency] 0.91)
+      (assoc-in [:material :material/refractive-index] 2.41)
+      (assoc-in [:material :material/reflective] 0.7)))
 
 (def light
   (light/point-light
    (tup/point -10 10 -10) col/white))
 
-(def width 800)
-(def height 500)
+(def width 300)
+(def height 200)
 (def field-of-view (/ Math/PI 3))
 (def camera-location (tup/point 0 1.5 -5))
 (def canvas-location (tup/point 0 1 0))
