802cbdcef8075cc728c673018c3ff8b2ba825ab5
Garrett Rowe <garrett.m.rowe@gmail.com>
2022-08-21 10:27:31 -0700

    Play around with cubes

--- src/racertay/core.clj
+++ src/racertay/core.clj
@@ -53,7 +53,7 @@
       (assoc-in [:material :material/color] col/dark-blue)
       (assoc-in [:material :material/diffuse] 0.7)
       (assoc-in [:material :material/specular] 0.3)
-      (assoc-in [:materail :material/reflective] 0.15)))
+      (assoc-in [:materail :material/reflective] 1.0)))
 
 (def middle-sphere
   (let [pattern (-> (patt/stripe-pattern col/violet col/white)
@@ -65,10 +65,10 @@
         (assoc-in [:material :material/diffuse] 0.7)
         (assoc-in [:material :material/specular] 0.3))))
 
-(def right-sphere
-  (-> (shape/sphere)
+(def right-cube
+  (-> (shape/cube)
       (shape/apply-transform
-       (xform/scaling 0.5 0.5 0.5)
+       (xform/scaling 0.25 1.5 0.25)
        (xform/translation 1.5 0.5 -0.5))
       (assoc-in [:material :material/color] col/black)
       (assoc-in [:material :material/diffuse] 0.5)
@@ -78,12 +78,13 @@
       (assoc-in [:material :material/refractive-index] 2.41)
       (assoc-in [:material :material/reflective] 0.7)))
 
+
 (def light
   (light/point-light
    (tup/point -10 10 -10) col/white))
 
-(def width 300)
-(def height 200)
+(def width 800)
+(def height 600)
 (def field-of-view (/ Math/PI 3))
 (def camera-location (tup/point 0 1.5 -5))
 (def canvas-location (tup/point 0 1 0))
@@ -101,7 +102,7 @@
       (update :world/objects conj left-wall)
       (update :world/objects conj right-wall)
       (update :world/objects conj middle-sphere)
-      (update :world/objects conj right-sphere)
+      (update :world/objects conj right-cube)
       (update :world/objects conj left-sphere)))
 
 (defn write-canvas-to-ppm-file [canvas filename]
